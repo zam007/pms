c31dd8d75f2a228e908859b7f20b3f3b38a42eae
zam <zam254227751@gmail.com>
2016-03-22 22:15:55 +0800

    de%%

--- Pms/Home/Controller/IndexController.class.php
+++ Pms/Home/Controller/IndexController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-         echo '你好，这是服务器的生产环境1，dell';
+         echo '你好，这是服务器的生产环境1，dell&proc_';
      }
     public function index1(){
          echo '你好，这是服务器的生产环境测试语句22';

b979d1dff377e38e188a5777c412cf6707917029
zam <zam254227751@gmail.com>
2016-03-22 16:32:07 +0800

    add push

--- Pms/Home/Controller/PushController.class.php
+++ Pms/Home/Controller/PushController.class.php
@@ -1,8 +1,8 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-class GitController extends Controller {
-    public function git(){
+class PushController extends Controller {
+    public function push(){
         error_reporting ( E_ALL );
 		$dir = '/www/test1/';//该目录为git检出目录
 		$handle = popen('cd '.$dir.' && git pull 2>&1','r');

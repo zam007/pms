1c20a60e0bb068ce6fa5a2358f692e9ca1d78e5c
zam <zam254227751@gmail.com>
2016-03-23 21:12:36 +0800

    change db sec

--- Pms/Home/Conf/config.php
+++ Pms/Home/Conf/config.php
@@ -6,7 +6,7 @@ return array(
 'DB_HOST'=>'112.74.73.22',// 服务器地址
 'DB_NAME'=>'pms',// 数据库名
 'DB_USER'=>'pms',// 用户名
-'DB_PWD'=>'Zy123456',// 密码
+'DB_PWD'=>'fqtmms136',// 密码
 'DB_PORT'=>3306,// 端口
 'DB_PREFIX'=>'think_',// 数据库表前缀
 'DB_CHARSET'=>'utf8',// 数据库字符集

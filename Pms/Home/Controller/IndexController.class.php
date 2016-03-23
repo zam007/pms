819f04a74d1badc0b681653357d8d315ec34dd85
zam <zam254227751@gmail.com>
2016-03-23 21:25:11 +0800

    kaishi

--- Pms/Home/Controller/IndexController.class.php
+++ Pms/Home/Controller/IndexController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-         echo '数据库设计阶段，敬请期待！';
+         echo '数据库设计阶段，敬请期待kaishi！';
      }
     public function index1(){
          echo '你好，这是服务器的生产环境测试语句22';

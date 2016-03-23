4a8e0c140bf436934aaf4e43b512ac81cbf4ecef
zam <zam254227751@gmail.com>
2016-03-23 15:54:13 +0800

    sucess

--- Pms/Home/Controller/IndexController.class.php
+++ Pms/Home/Controller/IndexController.class.php
@@ -3,7 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-         echo '你好ameiwoaini';
+         echo '你好,自动部署已经测试成功，可以投入使用！！！';
      }
     public function index1(){
          echo '你好，这是服务器的生产环境测试语句22';

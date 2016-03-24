535653f6f6b6522ba1d8d0c99227e7996152f5f5
zam <zam254227751@gmail.com>
2016-03-24 16:06:18 +0800

    rewrite

--- Pms/Common/Conf/config.php
+++ Pms/Common/Conf/config.php
@@ -2,4 +2,9 @@
 return array(
 'URL_ROUTER_ON'         => true,// 开启路由
 'URL_MODEL'             =>  2,//(REWRITE  模式)
+'URL_ROUTE_RULES' => array( //定义路由规则
+    'new/:name'    => 'Index/hello',
+
+),
+
 );

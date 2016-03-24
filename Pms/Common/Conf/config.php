ab8e36dc1fa0a7bfe23eae31ba7a4fec9dd9a025
zam <zam254227751@gmail.com>
2016-03-24 16:14:50 +0800

    ddddd

--- Pms/Common/Conf/config.php
+++ Pms/Common/Conf/config.php
@@ -2,9 +2,9 @@
 return array(
 'URL_ROUTER_ON'         => true,// 开启路由
 'URL_MODEL'             =>  2,//(REWRITE  模式)
-'URL_ROUTE_RULES' => array( //定义路由规则
-    'new/:name'    => 'Index/hello',
 
-),
+'URL_ROUTE_RULES' => array( //定义路由规则
+    'new/:name'    => '/Home/Index/hello',
+	),
 
 );

6d3d15dfa17e5302e3721ba8d2a10165525aac43
slowly-git <396236162@qq.com>
2016-04-25 18:01:33 +0800

    登录

--- Pms/Home/Controller/IndexController.class.php
+++ Pms/Home/Controller/IndexController.class.php
@@ -1,11 +1,15 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+use Home\IndexModel;
 class IndexController extends Controller {
     public function index(){
-         $this->display();
+         $this->display("login");
      }
      public function login(){
-         echo 123221;
+         $user["user_name"] = I("userName");
+         $user["password"] = md5(I("password").C("pwd_md5"));
+         $index = D("Index");
+         $index->login($user);
      }
 }

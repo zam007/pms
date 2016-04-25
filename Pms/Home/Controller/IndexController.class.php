234893553d48935e6e2d60685c41855fdf340bb3
slowly-git <396236162@qq.com>
2016-04-25 15:37:06 +0800

    修改文件位置错误

--- Pms/Home/Controller/IndexController.class.php
+++ Pms/Home/Controller/IndexController.class.php
@@ -2,7 +2,7 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-    public function index(){echo 343;
+    public function index(){
          $this->display();
      }
      public function login(){

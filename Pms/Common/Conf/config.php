0baa30a99a8b8dece5d92d4be8022bbcd78009ab
zhuanming <254227751@qq.com>
2016-04-27 13:44:15 +0800

    admin

--- Pms/Common/Conf/config.php
+++ Pms/Common/Conf/config.php
@@ -1,10 +1,15 @@
 <?php
 return array(
-'URL_ROUTER_ON'         => true,// 开启路由
-'URL_MODEL'             =>  2,//(REWRITE  模式)
-
-'URL_ROUTE_RULES' => array( //定义路由规则
-    'new/:name'    => 'Home/Index/hello',
-),
-'PWD_KEY'=>'wuxingcaishang',//加密字符
+	//设定默认访问目录
+	'DEFAULT_MODULE' => 'Home',
+	// 开启路由
+	'URL_ROUTER_ON'         => true,
+	//(REWRITE  模式)
+	'URL_MODEL'             =>  2,
+	//定义路由规则
+	'URL_ROUTE_RULES' => array(
+	    'new/:name'    => 'Home/Index/hello',
+	),
+	//加密字符
+	'PWD_KEY'=>'wuxingcaishang',
 );

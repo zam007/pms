85293a74c8b2c680df6673eda8b0ee749d137033
fengbo <fengbo@chcts.cn>
2016-05-17 15:56:53 +0800

    修改数据库地址

--- Pms/Home/Conf/config.php
+++ Pms/Home/Conf/config.php
@@ -2,7 +2,8 @@
 return array(
     // 添加数据库配置信息
     'DB_TYPE'=>'mysql',// 数据库类型
-    'DB_HOST'=>'112.74.73.22',// 服务器地址
+//    'DB_HOST'=>'112.74.73.22',// 服务器地址
+    'DB_HOST'=>'localhost',// 服务器地址
     'DB_NAME'=>'pms',// 数据库名
     'DB_USER'=>'pms',// 用户名
     'DB_PWD'=>'fqtmms136',// 密码

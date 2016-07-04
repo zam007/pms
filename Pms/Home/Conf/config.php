bf5b33f02f669ac703bd445e6c837bb6264201b9
zam <254227751@qq.com>
2016-07-04 23:09:14 +0800

    zam074

--- Pms/Home/Conf/config.php
+++ Pms/Home/Conf/config.php
@@ -10,7 +10,6 @@ return array(
     'DB_PORT'=>3306,// 端口
     'DB_PREFIX'=>'',// 数据库表前缀
     'DB_CHARSET'=>'utf8',// 数据库字符集
-    
     // 配置邮件发送服务器
     'MAIL_HOST' =>'smtp.qq.com',//smtp服务器的名称
     'MAIL_SMTPAUTH' =>TRUE, //启用smtp认证

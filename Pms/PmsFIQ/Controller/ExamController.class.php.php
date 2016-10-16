be03f9eba0c07eeb4d1697dc591fbfd948f73bc4
slowly-git <1435626505@qq.com>
2016-10-16 22:48:08 +0800

    auto

---
+++ Pms/PmsFIQ/Controller/ExamController.class.php.php
@@ -0,0 +1,10 @@
+<?php
+namespace PmsFIQ\Controller;
+use Think\Controller;
+class ExamController extends Controller {
+    
+    public function index(){
+        $page = I('page');
+        $search = I('search');
+    }
+}

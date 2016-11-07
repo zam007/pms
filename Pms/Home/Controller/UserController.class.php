39f1a27fbfb5e8efd364af6f9e1f91b578da6c3d
zam <zhuanming@tourweb.com>
2016-11-07 16:03:43 +0800

    修改index.htm和change_pwd.htm的表单提交功能

--- Pms/Home/Controller/UserController.class.php
+++ Pms/Home/Controller/UserController.class.php
@@ -88,12 +88,6 @@ class UserController extends BaseController {
      * 修改密码
      */
     public function updatePwd(){
-            //测试
-            $user['oldpassword'] = I("username");
-            $user['pwd'] = I("password");
-            $user['repwd']= I("repassword");
-            $this->ajaxReturn($user);die;
-
         $index = D("user");
         $userId = $this->userId;
         //修改成功后ajax返回信息

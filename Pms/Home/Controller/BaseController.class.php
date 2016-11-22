885e627022d6d9ddfcfb56b781bf22ab32d9fe76
zam <zhuanming@tourweb.com>
2016-11-22 16:34:55 +0800

    登陆修改

--- Pms/Home/Controller/BaseController.class.php
+++ Pms/Home/Controller/BaseController.class.php
@@ -9,7 +9,7 @@ class BaseController extends Controller {
     	parent::__construct();
          $userId = I('session.user_id',0);
          if($userId == 0){
-         	$this->error('您还未登录！','../Index/log');
+         	$this->error('您还未登录！','../Index/index');
          }
          $sessionId = I('session.session_id',0);
          // $lastSession = I('session.last_session_id',-1);

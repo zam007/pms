d10d29e019d2103d87f3e3bdba3bf286f5b357cf
slowly-git <1435626505@qq.com>
2017-02-08 00:11:42 +0800

    试题管理

--- Pms/Admin/Controller/UserController.class.php
+++ Pms/Admin/Controller/UserController.class.php
@@ -4,15 +4,16 @@ use Think\Controller;
 use Util\Util;
 class UserController extends BaseController {
     public function userList() {
-        $m = M('User');      
+        $model = M('User');      
         $where = array(
             'flag' => 1,
             );
-        $count = $m->where($where)->count();
+        $count = $model->where($where)->count();
         $p = getpage($count);
-        $list = $m->field(true)->where($where)->limit($p->firstRow, $p->listRows)->select();
+        $list = $model->field(true)->where($where)->limit($p->firstRow, $p->listRows)->select();
         $this->assign('list', $list); // 赋值数据集
         $this->assign('page', $p->show()); // 赋值分页输出
         $this->display();
+        die();
     }
 }
